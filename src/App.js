9681fcf045430b4987886374bdeb38a8adc6670f
abnaim33 <67532457+abnaim33@users.noreply.github.com>
2021-12-10 22:33:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import About from "./components/about/About"
+import Services from "./components/services/Services"
+import Projects from "./components/projects/Projects";
+import Footer from "./components/footer/Footer";
+import Contact from "./components/contact/Contact";
+import './style/style.css'
+import Skills from "./components/skills/Skills";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="scroll-up-btn">
+        <i className="fas fa-angle-up"></i>
+      </div>
+
+      <Navbar />
+
+      <Home />
+
+      <About />
+
+      <Services />
+
+      <Skills />
+
+      <Projects />
+
+      <Contact />
+
+      <Footer />
     </div>
+
   );
 }
 
